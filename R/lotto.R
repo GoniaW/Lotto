ecc6008243d076794760606d6667640f64030c08
Gonia Wachulec <gosia.wachulec@gmail.com>
2019-05-08 18:14:46 +0200

    dodanie opisow funcji

--- R/lotto.R
+++ R/lotto.R
@@ -1,6 +1,6 @@
 #' @title Lotto
 #'
-#' @description
+#' @description Draws as many numbers as specified in draw_number parameter from 49. Default is draw_numbers=6 like in standard lotto game, and the possible values of draw_numbers are integers from range 6-12 like in the various extentions of lotto game.
 #'
 #' @param draw_number
 #'
