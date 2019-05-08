3fda43c125dfc7370324250ad852b4703d0c1823
airi314 <olka.wichrowska@gmail.com>
2019-05-08 18:38:01 +0200

    dodanie strony

--- R/mini_lotto.R
+++ R/mini_lotto.R
@@ -2,10 +2,7 @@
 #'
 #' @description Like in standard Mini Lotto game 5 numbers are drawn at random from 42.
 #'
-#' @param draw_number
-#'
 #' @export
-
 
 mini_lotto <- function() {
   sample(42,5)
